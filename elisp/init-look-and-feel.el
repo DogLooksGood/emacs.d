41d0f4cd1187b9b03863547055cf8871952473e4
shitianshu <doglooksgood@gmail.com>
2020-10-30 17:48:40 +0800

    update

--- elisp/init-look-and-feel.el
+++ elisp/init-look-and-feel.el
@@ -9,8 +9,11 @@
 (defvar +current-theme nil
   "Name for current theme")
 
-(defvar +theme-list
-  '(joker storybook printed))
+(if window-system
+    (defvar +theme-list
+      '(joker storybook printed))
+  (defvar +theme-list
+    '(storybook joker printed)))
 
 (set-display-table-slot standard-display-table
                         'vertical-border
@@ -33,7 +36,7 @@
 
 ;; Fonts
 
-(let ((font "Victor Mono Medium-10"))
+(let ((font "Unifont-12"))
   (set-frame-font font)
   (add-to-list 'default-frame-alist (cons 'font font)))
 
