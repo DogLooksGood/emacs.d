bd7021e05274afd41275ca28c77f79f9292cb562
shitianshu <doglooksgood@gmail.com>
2020-11-12 00:39:22 +0800

    update

--- early-init.el
+++ early-init.el
@@ -4,10 +4,10 @@
 (setq gc-cons-threshold most-positive-fixnum
 	  gc-cons-percentage 0.6)
 
-(scroll-bar-mode -1)
+(scroll-bar-mode 1)
 (tool-bar-mode -1)
-(menu-bar-mode -1)
-(fringe-mode -1)
+(menu-bar-mode 1)
+(fringe-mode 1)
 (blink-cursor-mode -1)
 
 (add-to-list 'default-frame-alist '(internal-border-width . 15))
