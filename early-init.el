e318ffded50442421d82d3e0a55421d9c4e455ec
shitianshu <doglooksgood@gmail.com>
2020-11-14 14:35:40 +0800

    update

--- early-init.el
+++ early-init.el
@@ -4,9 +4,9 @@
 (setq gc-cons-threshold most-positive-fixnum
 	  gc-cons-percentage 0.6)
 
-(scroll-bar-mode 1)
+(scroll-bar-mode -1)
 (tool-bar-mode -1)
-(menu-bar-mode 1)
+(menu-bar-mode -1)
 (fringe-mode 1)
 (blink-cursor-mode -1)
 
