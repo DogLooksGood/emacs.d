711d4ed44702c814bb53c3aa4f207bcaf110220a
shitianshu <doglooksgood@gmail.com>
2020-11-20 00:20:18 +0800

    update

--- elisp/init-window.el
+++ elisp/init-window.el
@@ -6,4 +6,9 @@
   :custom
   (aw-keys '(?d ?h ?t ?n ?s)))
 
+(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-<down>") 'shrink-window)
+(global-set-key (kbd "S-C-<up>") 'enlarge-window)
+
 (provide 'init-window)
