ff0c660b47e54d77b370978d6ea9063332567e1c
shitianshu <doglooksgood@gmail.com>
2020-11-26 14:37:07 +0800

    update

--- elisp/init-font.el
+++ elisp/init-font.el
@@ -1,11 +1,23 @@
 ;;; -*- lexical-binding: t -*-
 
-(set-face-attribute 'default nil :font "IBM Plex Mono-10")
+(set-face-attribute 'default nil :font "Jetbrains Mono-11")
 
 (setq face-font-rescale-alist '(("等距更纱黑体 SC" . 1)))
 (dolist (charset '(kana han symbol cjk-misc bopomofo))
   (set-fontset-font (frame-parameter nil 'font)
                     charset
                     (font-spec :family "等距更纱黑体 SC")))
+
+(use-package ligature
+  :straight
+  (ligature :type git
+	        :host github
+	        :repo "mickeynp/ligature.el")
+  :config
+  (ligature-set-ligatures 'elixir-mode
+                          '("->" "=>" "|>" "<-" ">=" "<=" "!=" "=="))
+  (ligature-set-ligatures 'clojure-mode
+                          '("->" "->>" ">=" "<="))
+  (global-ligature-mode t))
 
 (provide 'init-font)
