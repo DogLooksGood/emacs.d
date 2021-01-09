6762213c66128e39a42d53eb4b33bf721c00b17b
shitianshu <doglooksgood@gmail.com>
2021-01-09 15:56:46 +0800

    update

--- elisp/init-misc.el
+++ elisp/init-misc.el
@@ -5,10 +5,11 @@
 (defun +update-window-divider ()
   "Only display window divider when there are more than two windows."
   (let ((n (length (window-list))))
-    (if (> n 1)
+    (if (or (meow-keypad-mode-p) (> n 1))
         (window-divider-mode 1)
       (window-divider-mode -1))))
 
 (add-hook 'window-configuration-change-hook '+update-window-divider)
+(add-hook 'meow-keypad-mode-hook '+update-window-divider)
 
 (provide 'init-misc)
