9d8a5c2796511a2381637ad192090aa0681b5d4d
shitianshu <doglooksgood@gmail.com>
2021-01-12 23:54:41 +0800

    update

--- elisp/init-laf.el
+++ elisp/init-laf.el
@@ -2,6 +2,9 @@
 
 (add-to-list 'load-path (expand-file-name "themes" user-emacs-directory))
 
+(defvar +after-change-theme-hook nil
+  "Hooks called after theme is changed.")
+
 (require 'joker-theme)
 (require 'storybook-theme)
 (require 'printed-theme)
@@ -28,7 +31,8 @@
     (let ((this-theme (car +theme-list)))
       (load-theme this-theme t)
       (unless no-msg
-        (message "Load theme: %s" this-theme)))))
+        (message "Load theme: %s" this-theme))
+      (run-hook-with-args '+after-change-theme-hook this-theme))))
 
 (defun +highlight-prog-mode-function-name ()
   (face-remap-add-relative 'font-lock-function-name-face :underline t :extend t))
--- elisp/init-org.el
+++ elisp/init-org.el
@@ -3,19 +3,43 @@
 (use-package org
   :straight (:type built-in))
 
-(defun +org-toggle-emphasis ()
-  "Toggle hiding/showing of org emphasize markers."
-  (interactive)
-  (if org-hide-emphasis-markers
-      (setq org-hide-emphasis-markers nil)
-    (setq org-hide-emphasis-markers t)))
+(defun +org-post-command-hook ()
+  (let ((in-latex (rime-predicate-org-latex-mode-p)))
+    (if (and +org-last-in-latex (not in-latex))
+        (progn (org-latex-preview)
+               (setq +org-last-in-latex nil)))
+
+    (when-let ((ovs (overlays-at (point))))
+      (when (->> ovs
+                 (--map (overlay-get it 'org-overlay-type))
+                 (--filter (equal it 'org-latex-overlay)))
+        (org-latex-preview)
+        (setq +org-last-in-latex t)))
+
+    (when in-latex
+      (setq +org-last-in-latex t))))
+
+(defun +org-latex-auto-toggle-setup ()
+  (add-hook 'post-command-hook '+org-post-command-hook nil t))
+
+(add-hook 'org-mode-hook '+org-latex-auto-toggle-setup)
+
+(defun +org-update-latex-option-by-theme (theme)
+  (setq org-format-latex-options
+        (plist-put org-format-latex-options :theme theme)))
+
+(add-hook '+after-change-theme-hook '+org-update-latex-option-by-theme)
 
 (use-package org-roam
   :hook
   (after-init . org-roam-mode)
   :custom
   (org-format-latex-options (plist-put org-format-latex-options :scale 4.0))
-  (org-roam-directory (expand-file-name "~/Org"))
+
+  (org-roam-directory
+   (let ((p (expand-file-name "~/Org")))
+     (unless (file-directory-p p) (make-directory p))
+     p))
   (org-roam-capture-templates
    '(("d" "default" plain (function org-roam--capture-get-point)
       "%?"
@@ -23,8 +47,6 @@
       :head "#+title: ${title}\n"
       :unnarrowed t)))
   :bind
-  (:map org-mode-map
-        ("C-c C-x RET" . +org-toggle-emphasis))
   (:map org-roam-mode-map
         ("C-c n l" . org-roam)
         ("C-c n f" . org-roam-find-file)
@@ -41,5 +63,7 @@
 
 (use-package org-superstar
   :hook (org-mode . org-superstar-mode))
+
+(defvar-local +org-last-in-latex nil)
 
 (provide 'init-org)
