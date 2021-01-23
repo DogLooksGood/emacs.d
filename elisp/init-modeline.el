fd611ffb12681d58fdea30274695d188ba4f5160
shitianshu <doglooksgood@gmail.com>
2021-01-23 23:58:25 +0800

    update

--- elisp/init-modeline.el
+++ elisp/init-modeline.el
@@ -9,8 +9,7 @@ Containing LEFT, and RIGHT aligned respectively."
   (let ((available-width
          (- (window-total-width)
             (+ (length (format-mode-line left))
-               (string-width (format-mode-line right)))
-            (- 1))))
+               (string-width (format-mode-line right))))))
     (append left
             (list (format (format "%%%ds" available-width) ""))
             right)))
