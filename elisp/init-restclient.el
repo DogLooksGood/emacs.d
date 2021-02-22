214c244b98205527c0a0c1cbd4004f29264b6a66
shitianshu <doglooksgood@gmail.com>
2021-02-22 11:09:41 +0800

    update

--- elisp/init-restclient.el
+++ elisp/init-restclient.el
@@ -1,10 +1,11 @@
 
-(use-package restclient)
+(straight-use-package 'restclient)
 
-(use-package ob-restclient
-  :config
-  (org-babel-do-load-languages
-   'org-babel-load-languages
-   '((restclient . t))))
+(add-to-list 'auto-mode-alist '("\\.restclient\\'" . restclient-mode))
+
+(straight-use-package 'ob-restclient)
+(require 'ob-restclient)
+
+(with-eval-after-load "ob-restclient" (org-babel-do-load-languages 'org-babel-load-languages '((restclient . t))))
 
 (provide 'init-restclient)
